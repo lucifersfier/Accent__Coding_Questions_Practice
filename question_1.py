82676524cb64e3355a5250cd598fc4530c847fc9
Lucifersfier <nssrivas@gmail.com>
2023-09-16 16:08:25 +0530

    Question number 1 successfully implemented

--- question_1.py
+++ question_1.py
@@ -9,3 +9,20 @@ Assumption
 o m > 0 and n > 0
 o Sum lies within the integral range'''
 #********************************************#
+sum1=0
+sum2=0
+def differenceofSum(n,m):
+    sum1=0
+    sum2=0
+    for i in range(n+1):
+        if(i%m==0):
+            sum1=sum1+i
+        else:
+            sum2=sum2+i
+    return sum2-sum1
+print(differenceofSum(30,6))
+print(differenceofSum(10,3))
+
+
+            
+        
