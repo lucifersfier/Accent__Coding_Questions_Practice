989ef7f0f48580f5e1902a155df4b1515273ec86
Lucifersfier <nssrivas@gmail.com>
2023-09-16 15:52:31 +0530

    Question Number - 1

---
+++ question_1.py
@@ -0,0 +1,11 @@
+# Question Number --1 
+'''1. Execute the given function.
+def differenceofSum(n.m)
+The function takes two integrals m and n as arguments. You are required to obtain the total of 
+all integers ranging between 1 to n (both inclusive) which are not divisible by m. You must 
+also return the distinction between the sum of integers not divisible by m with the sum of 
+integers divisible by m.
+Assumption
+o m > 0 and n > 0
+o Sum lies within the integral range'''
+#********************************************#
